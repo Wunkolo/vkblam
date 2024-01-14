429751d80e447e1eb9f30986f686d9d4943f93c5
Wunkolo <Wunkolo@gmail.com>
2024-01-13 18:09:52 -0800

    Tags/Bitmap: Create ImageView
    This is the wrong order to do things.
    Creating an ImageView requires the image to be bound to memory. But for now I am trying to move this bitmap-loading code out of Scene.cpp

--- source/VkBlam/Tags/Implementations/Bitmap.cpp
+++ source/VkBlam/Tags/Implementations/Bitmap.cpp
@@ -20,13 +20,20 @@ std::unique_ptr<Bitmap> Bitmap::LoadTag(
 	std::unique_ptr<Bitmap> NewBitmap(new Bitmap());
 	const Vulkan::Context&  VulkanContext = TargetScene.GetVulkanContext();
 
-	const auto SubTextures
+	const auto SubBitmaps
 		= TargetScene.GetMapFile().TagHeap.GetBlock(Tag.Bitmaps);
 
-	for( std::size_t CurSubTextureIdx = 0;
-		 CurSubTextureIdx < SubTextures.size(); ++CurSubTextureIdx )
+	NewBitmap->Bitmaps.resize(SubBitmaps.size());
+
+	for( std::size_t CurSubTextureIdx = 0; CurSubTextureIdx < SubBitmaps.size();
+		 ++CurSubTextureIdx )
 	{
-		const auto& CurBitmapEntry = SubTextures[CurSubTextureIdx];
+		const auto& CurBitmapEntry = SubBitmaps[CurSubTextureIdx];
+
+		const std::size_t MipCount
+			= std::max<std::uint16_t>(CurBitmapEntry.MipmapCount, 1);
+		const std::size_t LayerCount
+			= CurBitmapEntry.Type == Blam::BitmapEntryType::CubeMap ? 6 : 1;
 
 		// Create Image handles
 		vk::ImageCreateInfo ImageInfo = {};
@@ -74,7 +81,65 @@ std::unique_ptr<Bitmap> Bitmap::LoadTag(
 			TargetScene.GetMapFile().GetTagName(TagIndexEntry.TagID)
 		);
 
-		NewBitmap->Bitmaps.emplace_back(std::move(CurSubBitmap));
+		//// Image must be fully bound before creating an ImageView
+
+		// Create Image View
+		vk::ImageViewCreateInfo BitmapImageViewInfo = {};
+		BitmapImageViewInfo.image                   = CurSubBitmap.Image.get();
+		switch( CurBitmapEntry.Type )
+		{
+		default:
+		case Blam::BitmapEntryType::Texture2D:
+		{
+			BitmapImageViewInfo.viewType = vk::ImageViewType::e2D;
+			break;
+		}
+		case Blam::BitmapEntryType::Texture3D:
+		{
+			BitmapImageViewInfo.viewType = vk::ImageViewType::e3D;
+			break;
+		}
+		case Blam::BitmapEntryType::CubeMap:
+		{
+			BitmapImageViewInfo.viewType = vk::ImageViewType::eCube;
+			break;
+		}
+		}
+		BitmapImageViewInfo.format = VkBlam::BlamToVk(CurBitmapEntry.Format);
+		;
+		BitmapImageViewInfo.components.r = vk::ComponentSwizzle::eR;
+		BitmapImageViewInfo.components.g = vk::ComponentSwizzle::eG;
+		BitmapImageViewInfo.components.b = vk::ComponentSwizzle::eB;
+		BitmapImageViewInfo.components.a = vk::ComponentSwizzle::eA;
+		BitmapImageViewInfo.subresourceRange.aspectMask
+			= vk::ImageAspectFlagBits::eColor;
+		BitmapImageViewInfo.subresourceRange.baseMipLevel   = 0;
+		BitmapImageViewInfo.subresourceRange.levelCount     = MipCount;
+		BitmapImageViewInfo.subresourceRange.baseArrayLayer = 0;
+		BitmapImageViewInfo.subresourceRange.layerCount     = LayerCount;
+
+		if( auto CreateResult = VulkanContext.LogicalDevice
+									.createImageViewUnique(BitmapImageViewInfo);
+			CreateResult.result == vk::Result::eSuccess )
+		{
+			CurSubBitmap.View = std::move(CreateResult.value);
+		}
+		else
+		{
+			std::fprintf(
+				stderr, "Error creating bitmap view: %s\n",
+				vk::to_string(CreateResult.result).c_str()
+			);
+		}
+
+		Vulkan::SetObjectName(
+			VulkanContext.LogicalDevice, CurSubBitmap.View.get(),
+			"Bitmap View {:08X}[{:2}] | {}", TagIndexEntry.TagID,
+			CurSubTextureIdx,
+			TargetScene.GetMapFile().GetTagName(TagIndexEntry.TagID)
+		);
+
+		NewBitmap->Bitmaps[CurSubTextureIdx] = std::move(CurSubBitmap);
 	}
 
 	return NewBitmap;
