defa26bc0b0cfee986962f62a5a02e1b2a4fe139
Wunkolo <Wunkolo@gmail.com>
2024-09-24 21:21:40 -0700

    Init `Globals` Tag subsystem
    Simply refers to the default texture images

--- include/VkBlam/Tags/TagImplementations.hpp
+++ include/VkBlam/Tags/TagImplementations.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "./Implementations/Bitmap.hpp"
+#include "./Implementations/Globals.hpp"
 #include "./Implementations/Scenario.hpp"
 #include "./Implementations/ScenarioStructureBsp.hpp"
 #include "./Implementations/ShaderEnvironment.hpp"
