3784a80d22c36dedd74a53c4af073d67aad947ca
Wunkolo <Wunkolo@gmail.com>
2024-12-14 12:15:43 -0800

    VkBlam: Refactor bitmap allocator
    Use `std::optional`-based allocations.
    Implement edge-case where incoming allocation is
    larger than the block-size.

--- include/VkBlam/Tags/Implementations/Bitmap.hpp
+++ include/VkBlam/Tags/Implementations/Bitmap.hpp
@@ -43,14 +43,19 @@ private:
 
 	// All of these images are static textures that only ever get sample from,
 	// so a simple heap-allocator is utilized
+	// Minimum requirements for Halo CE is 64MiB of VRAM, so this should
+	// be a good block-size
 	static constexpr std::uint64_t BlockSize = 64_MiB;
-	static_assert(std::has_single_bit(BlockSize), "BlockSize must be pow2");
+	static_assert(
+		std::has_single_bit(BlockSize), "BlockSize must be a power of two"
+	);
 
 	std::vector<std::uint32_t>          BlockFreeSpace;
 	std::vector<std::uint8_t>           BlockMemoryIndex;
 	std::vector<vk::UniqueDeviceMemory> BlockMemory;
 
-	vk::BindImageMemoryInfo FindFreeBlock(vk::Image);
+	// Todo: std::expected
+	std::optional<vk::BindImageMemoryInfo> FindFreeBlock(vk::Image Image);
 
 	std::vector<std::unique_ptr<Bitmap>> Bitmaps;
 
