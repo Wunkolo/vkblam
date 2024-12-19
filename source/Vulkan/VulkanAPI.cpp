a3f50584052061cf9805ff64c1439b8b28e6f965
Wunkolo <Wunkolo@gmail.com>
2024-12-19 08:12:23 -0800

    Vulkan: Use `detail`-namespace when header `>=303`
    Remove this once the Lunarg SDK releases beyond this version number.
    Currently Lunarg is at 296.

--- source/Vulkan/VulkanAPI.cpp
+++ source/Vulkan/VulkanAPI.cpp
@@ -3,7 +3,16 @@
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE;
 
 static const bool VulkanLoader = []() -> bool {
+
+// Remove this once the Lunarg SDK releases beyond this version number.
+// Currently(12/19/2024) Lunarg is at 296.
+// https://github.com/KhronosGroup/Vulkan-Hpp/pull/1983
+#if VK_HEADER_VERSION >= 303
 	static vk::detail::DynamicLoader DynamicLoader;
+#else
+	static vk::DynamicLoader DynamicLoader;
+#endif
+
 	VULKAN_HPP_DEFAULT_DISPATCHER.init(
 		DynamicLoader.getProcAddress<PFN_vkGetInstanceProcAddr>(
 			"vkGetInstanceProcAddr"
